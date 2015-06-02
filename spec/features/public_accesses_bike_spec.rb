a9d48a787ff18806372cf1d410ab66270b969b32
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-02 13:43:46 +0100

    completed stage2

---
+++ spec/features/public_accesses_bike_spec.rb
@@ -0,0 +1,8 @@
+feature 'member of public accesses bike' do
+  scenario 'docking station releases a working bike' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike).to be_working #expect the bike to respond to the method working? with true
+  end
+
+end

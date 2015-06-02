06bb45684009cff0c01c0e3c1e07b084e18743ca
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-02 21:06:40 +0100

    refactored stage5

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,18 +2,38 @@ require_relative 'bike'
 
 class DockingStation
 
+  DEFAULT_CAPACITY = 20
+
+  attr_reader :capacity
+
   def initialize
     @bikes = []
+    @capacity = DEFAULT_CAPACITY
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock bike
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if full?
     @bikes << bike
   end
+
+
+  private
+
+  attr_reader :bikes
+
+  def empty?
+    bikes.empty?
+  end
+
+  def full?
+    bikes.count >= capacity
+  end
+
+
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -11,6 +11,11 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+
+  end
+
   describe 'release_bike' do
     it 'raises an error when no bikes are available' do
        expect { subject.release_bike}.to raise_error 'No bikes available'
